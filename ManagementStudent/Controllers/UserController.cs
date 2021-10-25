13548392299a5c0a34c6a0f77a2b3538fcc6b5cc
Minh nha <nguyenminhnhacmu@gmail.com>
2021-10-25 23:11:30 +0700

    'new'

---
+++ ManagementStudent/Controllers/UserController.cs
@@ -0,0 +1,130 @@
+﻿using ManagementStudent.Models;
+using ManagementStudent.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManagementStudent.Controllers
+{
+    public class UserController : Controller
+    {
+        UserRepository userRepository = new UserRepository();
+        // GET: User
+        public ActionResult Index(string msg)
+        {
+            var listStudent = userRepository.getListStudent();
+            ViewBag.MSG = msg;
+            return View(listStudent);
+        }
+
+       
+
+        public ActionResult ListGiangVien(string msg)
+        {
+            ViewBag.MSG = msg;
+            var listGiangVien = userRepository.getListGiangVien();
+            return View(listGiangVien);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(FormCollection form)
+        {
+            var id = form["id"];
+            userRepository.delete(Int32.Parse(id));
+            return RedirectToAction("Index", new { msg = "1" });
+        }
+
+        [HttpPost]
+        public ActionResult Add(FormCollection form)
+        {
+            User principal = new User(); 
+
+            principal.fullname = form["fullname"];
+            principal.username = form["username"];
+            principal.password = form["password"];
+            principal.id_role  = Int32.Parse(form["id_role"]);
+            principal.gender = Int32.Parse(form["gender"]);
+            principal.address = form["address"];
+            principal.grade = form["grade"];
+            principal.status = 1;
+            var obj = userRepository.checkExistName(form["username"]);
+            if (obj == null)
+            {
+                userRepository.add(principal);
+                return RedirectToAction("Index", new { msg = "1" });
+            }
+            else
+            {
+                return RedirectToAction("Index", new { msg = "2" });
+            }
+           
+        }
+
+        [HttpPost]
+        public ActionResult AddGiangVien(FormCollection form)
+        {
+            User principal = new User();
+
+            principal.fullname = form["fullname"];
+            principal.username = form["username"];
+            principal.password = form["password"];
+            principal.id_role = Int32.Parse(form["id_role"]);
+            principal.gender = Int32.Parse(form["gender"]);
+            principal.address = form["address"];
+            principal.grade = "null";
+            principal.status = 1;
+            var obj = userRepository.checkExistName(form["username"]);
+            if (obj == null)
+            {
+                userRepository.add(principal);
+                return RedirectToAction("ListGiangVien", new { msg = "1" });
+            }
+            else
+            {
+                return RedirectToAction("ListGiangVien", new { msg = "2" });
+            }
+
+        }
+
+
+        [HttpPost]
+        public ActionResult Update(FormCollection form)
+        {
+            User principal = new User();
+
+            principal.id_user = Int32.Parse(form["id"]);
+            principal.fullname = form["fullname"];
+            principal.username = form["username"];
+            principal.password = form["password"];
+            principal.id_role = Int32.Parse(form["id_role"]);
+            principal.gender = Int32.Parse(form["gender"]);
+            principal.address = form["address"];
+            principal.grade = form["grade"];
+            principal.status = 1;
+            userRepository.edit(principal);
+            return RedirectToAction("Index", new { msg = "1" });          
+
+        }
+
+        [HttpPost]
+        public ActionResult UpdateGiangVien(FormCollection form)
+        {
+            User principal = new User();
+
+            principal.id_user = Int32.Parse(form["id"]);
+            principal.fullname = form["fullname"];
+            principal.username = form["username"];
+            principal.password = form["password"];
+            principal.id_role = Int32.Parse(form["id_role"]);
+            principal.gender = Int32.Parse(form["gender"]);
+            principal.address = form["address"];
+            principal.grade = form["grade"];
+            principal.status = 1;
+            userRepository.edit(principal);
+            return RedirectToAction("ListGiangVien", new { msg = "1" });
+
+        }
+    }
+}
--- ManagementStudent/Repositories/UserRepository.cs
+++ ManagementStudent/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ManagementStudent.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,52 @@ namespace ManagementStudent.Repositories
 {
     public class UserRepository
     {
+        ManageDbContext myDb = new ManageDbContext();
+
+        public List<User> getListStudent()
+        {
+            return myDb.users.Where(x => x.Role.id_role == 3).ToList();
+        }
+
+        public List<User> getListGiangVien()
+        {
+            return myDb.users.Where(x => x.Role.id_role == 2).ToList();
+        }
+
+        public void add(User user)
+        {
+            myDb.users.Add(user);
+            myDb.SaveChanges();
+        }
+
+        public void edit(User user)
+        {
+            var obj = myDb.users.FirstOrDefault(x => x.id_user == user.id_user);
+            obj.id_role = user.id_role;
+            obj.username = user.username;
+            obj.password = user.password;
+            obj.grade = user.grade;
+            obj.gender = user.gender;  
+            obj.address = user.address;
+            obj.fullname = user.fullname;
+            myDb.SaveChanges();
+        }
+
+        public void delete(int id)
+        {
+            var obj = myDb.users.FirstOrDefault(x => x.id_user == id);
+            myDb.users.Remove(obj);
+            myDb.SaveChanges();
+        }
+
+        public User checkExistName(string name)
+        {
+            return myDb.users.FirstOrDefault(x => x.username == name);
+        }
+       
+        public User getUserById(int id)
+        {
+            return myDb.users.FirstOrDefault(x => x.id_user == id);
+        }
     }
 }
